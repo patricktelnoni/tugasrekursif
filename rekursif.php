e75da09b53a9a935977c1abe0161b6df6c7e29cd
Patrick Telnoni <ptelnoni89@gmail.com>
2018-09-14 08:11:11 +0700

    Jawaban Tugas Rekursif

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,23 @@
 <?php
-
-function rekursif($i){
-  echo "* <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+$iterator1 = 0;
+$iterator2 = 0;
+function rekursifBaris($i){    
+    global $iterator1;
+    $iterator1++;
+    rekursifKolom($iterator1);
+    echo "<br>";    
+    if($iterator1<$i)
+        rekursifBaris($i);
 }
-rekursif(5);
+function rekursifKolom($j){
+    global $iterator2;
+    $iterator2++;
+    echo "$iterator2";    
+    if($iterator2<$j)
+        rekursifKolom($j);
+    else 
+        $iterator2 = 0;
+}
+rekursifBaris(5);
+
 ?>
